750bb7794e13c653f6be9d429795a00636e88752
thierry <thierrymoumi@gmail.com>
2020-03-28 19:28:16 +0100

    yes

--- utils.py
+++ utils.py
@@ -3,39 +3,43 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
-from Math import *
-from scipy import *
+from math import *
+import scipy.integrate as integrat
+from scipy.integrate import quad
+import scipy.special as special
 
 def fact(n):
-	"""Computes the factorial of a natural number.
-	
-	Pre: -
-	Post: Returns the factorial of 'n'.
-	Throws: ValueError if n < 0
-	"""
-	if type(n)!=int:
-		raise ValueError("Not Found")
-	if n<0:
-		raise ValueError("error")
-	if n==0:
-        return 1
-    else:
-        return n*fact(n-1) 
+	if type(n) != int:
+		raise TypeError
+	if n< 0:
+		raise  ValueError()
+	if n in range(0,2):
+		return 1
+	else :
+		return n*fact(n-1)
 	pass
 
 def roots(a, b, c):
-	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
-	
-	Pre: -
-	Post: Returns a tuple with zero, one or two elements corresponding
-		to the roots of the ax^2 + bx + c polynomial.
-	"""
-	def f(x):
-		return eval(function)
-
-	result 
+	delta = (b**2)-(4*a*c)
+	if type(a) != int:
+		raise TypeError
+	if type(b) != int:
+		raise TypeError
+	if type(c) != int:
+		raise TypeError
+	if delta>0:
+		x1 = (-b+sqrt(delta))/(2*a)
+		x2 = (-b-sqrt(delta))/(2*a)
+		return x1,x2
+	if delta<0:
+		return 'No roots'
+	if delta==0:
+		x = (-b)/2*a
+		return x
+	pass
 
 def integrate(function, lower, upper):
+
 	"""Approximates the integral of a fonction between two bounds
 	
 	Pre: 'function' is a valid Python expression with x as a variable,
@@ -43,16 +47,14 @@ def integrate(function, lower, upper):
 		'function' continuous and integrable between 'lower‘ and 'upper'.
 	Post: Returns an approximation of the integral from 'lower' to 'upper'
 		of the specified 'function'.
-
 	Hint: You can use the 'integrate' function of the module 'scipy' and
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-
-	integral = scipy.integrate.quad(function,lower,upper)
-	pass
+	def f(x):
+		return eval(function)
 
 if __name__ == '__main__':
-	print(fact(5))
-	print(roots(1, 0, 1))
+	print(fact(-1))
+	print(roots(1, 5, 2))
 	print(integrate('x ** 2 - 1', -1, 1))
